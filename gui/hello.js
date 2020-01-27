0058b16a9d6ad1057cfa08675fba7787239cc0a8
milkyklim <10698619+milkyklim@users.noreply.github.com>
2020-01-27 15:47:35 +0100

    add simple micro blog tutorial code

--- gui/hello.js
+++ gui/hello.js
@@ -1,5 +1,15 @@
 var holochain_connection = holochainclient.connect();
 
+function get_agent_id() {
+  holochain_connection.then(({ callZome, close }) => {
+    callZome(
+      "test-instance",
+      "into_pieces",
+      "get_agent_id",
+    )({}).then(result => show_output(result, "agent_id"));
+  });
+}
+
 function show_output(result, id) {
   var el = document.getElementById(id);
   var output = JSON.parse(result);
@@ -11,12 +21,19 @@ function show_output(result, id) {
   }
 }
 
-function show_person(result) {
-  var person = document.getElementById("person_output");
-  var output = JSON.parse(result);
+function display_posts(result) {
+  var list = document.getElementById("posts_output");
+  list.innerHTML = "";
 
+  var output = JSON.parse(result);
   if (output.Ok) {
-    person.textContent = " " + output.Ok.name;
+    var posts = output.Ok.sort((a, b) => a.timestamp - b.timestamp);
+    for (post of posts) {
+      var node = document.createElement("LI");
+      var textnode = document.createTextNode(post.message);
+      node.appendChild(textnode);
+      list.appendChild(node);
+    }
   } else {
     alert(output.Err.Internal);
   }
@@ -32,28 +49,28 @@ function hello() {
   });
 }
 
-function create_person() {
-  const name = document.getElementById("name").value;
+function create_post() {
+  const message = document.getElementById("post").value;
+  const timestamp = Date.now();
   holochain_connection.then(({ callZome, close }) => {
     callZome(
       "test-instance",
       "into_pieces",
-      "create_person",
+      "create_post",
     )({
-      person: { name: name },
+      message: message,
+      timestamp: timestamp,
     }).then(result => show_output(result, "address_output"));
   });
 }
 
-function retrieve_person() {
+function retrieve_posts() {
   var address = document.getElementById("address_in").value;
   holochain_connection.then(({ callZome, close }) => {
     callZome(
       "test-instance",
       "into_pieces",
-      "retrieve_person",
-    )({ address: address }).then(result =>
-      show_person(result, "person_output"),
-    );
+      "retrieve_posts",
+    )({ agent_address: address }).then(result => display_posts(result));
   });
 }
